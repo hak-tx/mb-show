d9627f8e0e45e82fd81a31af8ea3ee0a58698bc5
hak-tx <hakelbrian@gmail.com>
2026-06-24 13:52:12 -0500

    Point sponsor directory to Supabase

--- config.js
+++ config.js
@@ -1,8 +1,15 @@
 window.MB_SHOW_SUPABASE = {
-  url: "",
-  publishableKey: "",
+  url: "https://meilwvvsqnrbreuyaqbh.supabase.co",
+  publishableKey: "sb_publishable_trdHe45uPkvXjSbDvmfTAQ_MVyC_jA6",
 };
 
 window.MB_SHOW_ANALYTICS = {
   measurementId: "G-GYJ49804HS",
+};
+
+window.MB_SHOW_SHOPIFY = {
+  storeOrigin: "https://shop.michaelberryshow.com",
+  newReleasesEndpoint: "/api/new-releases",
+  newReleasesUrl: "https://shop.michaelberryshow.com/collections/new-releases",
+  productLimit: 8,
 };
